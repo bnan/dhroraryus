5d2836920f6efb9d39e29c6eb9b8b83f6c619b09
Luís Silva <lfssilva@ua.pt>
2017-09-08 22:56:06 +0100

    Prune code

--- src/App.js
+++ src/App.js
@@ -29,39 +29,50 @@ class App extends Component {
     const ACAP = new Event('ACA-P');
     const EDCT = new Event('EDC-T');
     const EDCP = new Event('EDC-P');
-    const SEGT = new Event('EDC-T');
-    const SEGP = new Event('EDC-P');
+    const SEGT = new Event('SEG-T');
+    const SEGP = new Event('SEG-P');
     const CVT = new Event('CV-T');
     const CVP = new Event('CV-P');
 
-    const ARAT1 = new EventOption(ARAT,1,[ new EventOptionInstance( new Date(3,13,0) , new Date(3,15,0)), ])
+    const ARAT1 = new EventOption(ARAT, 1,[ new EventOptionInstance( new Date(3,13,0) , new Date(3,15,0)), ]);
+    const ARAP1 = new EventOption(ARAP, 1,[ new EventOptionInstance( new Date(2,15,0) , new Date(2,17,0)), ]);
+    const ARAP2 = new EventOption(ARAP, 2,[ new EventOptionInstance( new Date(5,11,0) , new Date(5,13,0)), ]);
+    const ARAP3 = new EventOption(ARAP, 3,[ new EventOptionInstance( new Date(2,17,0) , new Date(2,19,0)), ]);
+    const ARAP4 = new EventOption(ARAP, 4,[ new EventOptionInstance( new Date(5, 9,0) , new Date(5,11,0)), ]);
 
-    const ARAP1 = new EventOption(ARAP, 1,[ new EventOptionInstance( new Date(2,15,0) , new Date(2,17,0)), ])
-    const ARAP2 = new EventOption(ARAP, 2,[ new EventOptionInstance( new Date(5,11,0) , new Date(5,13,0)), ])
-    const ARAP3 = new EventOption(ARAP, 3,[ new EventOptionInstance( new Date(2,17,0) , new Date(2,19,0)), ])
-    const ARAP4 = new EventOption(ARAP, 4,[ new EventOptionInstance( new Date(5, 9,0) , new Date(5,11,0)), ])
+    const ACAP1 = new EventOption(ACAP, 1,[ new EventOptionInstance( new Date(2,11,0) , new Date(2,13,0)), ]);
+    const ACAP2 = new EventOption(ACAP, 2,[ new EventOptionInstance( new Date(6,14,0) , new Date(6,16,0)), ]);
+    const ACAP3 = new EventOption(ACAP, 3,[ new EventOptionInstance( new Date(2, 9,0) , new Date(2,11,0)), ]);
+    const ACAP4 = new EventOption(ACAP, 4,[ new EventOptionInstance( new Date(2,14,0) , new Date(2,16,0)), ]);
+    const ACAT1 = new EventOption(ACAT, 1,[ new EventOptionInstance( new Date(4,10,0) , new Date(4,12,0)), ]);
 
-    const ACAP1 = new EventOption(ACAP, 1,[ new EventOptionInstance( new Date(2,11,0) , new Date(2,13,0)), ])
-    const ACAP2 = new EventOption(ACAP, 2,[ new EventOptionInstance( new Date(6,14,0) , new Date(6,16,0)), ])
-    const ACAP3 = new EventOption(ACAP, 3,[ new EventOptionInstance( new Date(2, 9,0) , new Date(2,11,0)), ])
-    const ACAP4 = new EventOption(ACAP, 4,[ new EventOptionInstance( new Date(2,14,0) , new Date(2,16,0)), ])
-    const ACAT1 = new EventOption(ACAT,1,[ new EventOptionInstance( new Date(4,10,0) , new Date(4,12,0)), ])
+    const CVP1 = new EventOption(CVP, 1,[ new EventOptionInstance( new Date(2,15,0), new Date(2,17,0)), ]);
+    const CVP2 = new EventOption(CVP, 2,[ new EventOptionInstance( new Date(2,13,0), new Date(2,15,0)), ]);
+    const CVP3 = new EventOption(CVP, 3,[ new EventOptionInstance( new Date(5,11,0), new Date(5,13,0)), ]);
+    const CVT1 = new EventOption(CVT, 1,[ new EventOptionInstance( new Date(3,15,0), new Date(3,17,0)), ]);
 
-    let cl = [ARAT1, ARAP1, ARAP2, ARAP3, ARAP4, ACAT1, ACAP1, ACAP2, ACAP3, ACAP4]
+    const EDCP1 = new EventOption(EDCP , 1,[ new EventOptionInstance( new Date(6, 9, 0), new Date(6,11, 0)), ]);
+    const EDCP2 = new EventOption(EDCP , 2,[ new EventOptionInstance( new Date(5,16,30), new Date(5,18,30)), ]);
+    const EDCP3 = new EventOption(EDCP , 3,[ new EventOptionInstance( new Date(6,11, 0), new Date(6,13, 0)), ]);
+    const EDCT1 = new EventOption(EDCT , 1,[ new EventOptionInstance( new Date(5,14,30), new Date(5,16,30)), ]);
 
-    var domain = new Map();
+    const SEGP1 = new EventOption(SEGP , 1,[ new EventOptionInstance( new Date(5, 9,0), new Date(5,11,0)), ]);
+    const SEGP2 = new EventOption(SEGP , 2,[ new EventOptionInstance( new Date(6,11,0), new Date(6,13,0)), ]);
+    const SEGP3 = new EventOption(SEGP , 3,[ new EventOptionInstance( new Date(6, 9,0), new Date(6,11,0)), ]);
+    const SEGP4 = new EventOption(SEGP , 4,[ new EventOptionInstance( new Date(6,14,0), new Date(6,16,0)), ]);
+    const SEGT1 = new EventOption(SEGT , 1,[ new EventOptionInstance( new Date(4,13,0), new Date(4,15,0)), ]);
 
-    for ( var c in cl ){
-      if ( !domain.has(cl[c].event.name)) {
-        domain.set(cl[c].event.name, [cl[c],]);
-      }
-      else {
-        let value = domain.get(cl[c].event.name);
-        value.push(cl[c]);
-        domain.set(cl[c].event.name, value);
-      }
-    }
-    let sol = search(domain, [overlapConstraint])
+    let cl = [
+              ARAT1, ARAP1, ARAP2, ARAP3, ARAP4,
+              ACAT1, ACAP1, ACAP2, ACAP3, ACAP4,
+              CVT1, CVP1, CVP2, CVP3,
+              EDCT1, EDCP1, EDCP2, EDCP3,
+              SEGT1, SEGP1, SEGP2, SEGP3, SEGP4
+              ]
+
+    let domain = createDomain(cl)
+
+    let sol = search(domain)
     console.log(sol)
   }
 
@@ -69,12 +80,30 @@ class App extends Component {
 
 }
 
+// Returns domain of eventOptions
+function createDomain(eventOptions)
+{
+  let domain = new Map();
+  let value = NaN;
+  console.log(eventOptions)
+  for ( var c in eventOptions ){
+    if ( !domain.has(eventOptions[c].event.name)) {
+      domain.set(eventOptions[c].event.name, [eventOptions[c],]);
+    }
+    else {
+      value = domain.get(eventOptions[c].event.name);
+      value.push(eventOptions[c]);
+      domain.set(eventOptions[c].event.name, value);
+    }
+  }
+  console.log(domain)
+  return domain
+}
+
 
 // Returns dictionaries with the solutions
-function search(domain, constraints)
+function search(domain)
 {
-
-  console.log(domain)
   // Return null if there is no solution
   if (Array.from(domain.values()).some(v => v.length == 0)) {
     return null
@@ -110,15 +139,12 @@ function search(domain, constraints)
       newDomain.set(key2, tmp)
     }
 
-    const s = search(newDomain, constraints)
+    const s = search(newDomain)
 
     if (s != null) {
       solutions = solutions.concat(s)
     }
   }
-  
-  console.log('Sol')
-  console.log(solutions)
   return solutions
 }
 
@@ -126,16 +152,7 @@ function overlapConstraint(c1, c2)
 {
   for (const e1 of c1.instances) {
     for (const e2 of c2.instances) {
-      console.log("BLLLLLLLLLLLLLLLLLLLLLAAAAAAAAAAAAAAAAAAAAA");
-      console.log("e1.start:" + e1.start);
-      console.log("e1.end:" + e1.end);
-      console.log("e2.start:" + e2.start);
-      console.log("e2.end:" + e2.end);
-      
-
-
-      if ((Date.compare(e1.start, e2.end) < 0) && (Date.compare(e1.end, e2.start) > 0)) {
-        console.log("TRUUUUE");
+      if ((Date.compare(e1.start, e2.end) < 0)) {
         return true
       }
     }
