1692a26c683634e825133ed774062bf6f33a7345
Luís Silva <lfssilva@ua.pt>
2017-09-09 12:58:10 +0100

    Revert for real

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {test} from './cSearch';
+
+import {test} from './cSearch'
 
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    test()
+    test();
     return (
       <div className="App">
       <div className="App-header">
--- src/cSearch.js
+++ src/cSearch.js
@@ -1,49 +1,39 @@
 import {WeekDate, Event, EventOption, EventOptionInstance} from './suppClasses'
 
-// Returns eventOption
-export function createEventOption(eventName, optionID, start_day, start_hour, start_min, end_day, end_hour, end_min)
+
+function overlapConstraint(c1, c2)
 {
-// TODO
-  //
+  for (const e1 of c1.instances) {
+    for (const e2 of c2.instances) {
+      if ((WeekDate.compare(e1.start, e2.end) < 0)) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
-
 // Returns domain of eventOptions
-export function makeDomain(events, domain)
+function makeDomain(eventOptions, domain)
 {
   if ( typeof domain == 'undefined' )
     domain = new Map();
 
   let value = NaN;
 
-  console.log(events)
-  for ( var c in events ){
-    console.log(c)
-    if ( !domain.has(events[c])) {
-      domain.set(events[c], events[c].options);
+  console.log(eventOptions)
+  for ( var c in eventOptions ){
+    if ( !domain.has(eventOptions[c].event.name)) {
+      domain.set(eventOptions[c].event.name, [eventOptions[c],]);
     }
     else {
-      value = domain.get(events[c]);
-      value.push(events[c].options);
-      domain.set(events[c],value);
+      value = domain.get(eventOptions[c].event.name);
+      value.push(eventOptions[c]);
+      domain.set(eventOptions[c].event.name, value);
     }
   }
   console.log(domain)
   return domain
-}
-
-export function overlapConstraint(c1, c2)
-{
-  for (const e1 of c1.instances) {
-    for (const e2 of c2.instances) {
-      console.log(c1,c2)
-      if ((WeekDate.compare(e1.start, e2.end) < 0)) {
-        return true
-      }
-    }
-  }
-
-  return false
 }
 
 // Returns dictionaries with the solutions
@@ -63,8 +53,9 @@ export function search(domain)
   const keys = Array.from(domain.keys()).concat().sort(e => domain.get(e).length)
 
   const key = keys.filter( k => domain.get(k).length > 1)[0]
-
+  // Missing picking value by most constraints created
   let solutions = []
+
   for (const value of domain.get(key)) {
     // Copy map
     const newDomain = new Map(domain)
@@ -103,46 +94,45 @@ export function test(){
     const CVT = new Event('CV-T');
     const CVP = new Event('CV-P');
 
-    ARAT.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(3,13,0) , new WeekDate(3,15,0)), ]), ];
-    ARAP.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(2,15,0) , new WeekDate(2,17,0)), ]),
-                    new EventOption('2',[ new EventOptionInstance( new WeekDate(5,11,0) , new WeekDate(5,13,0)), ]),
-                    new EventOption('3',[ new EventOptionInstance( new WeekDate(2,17,0) , new WeekDate(2,19,0)), ]),
-                    new EventOption('4',[ new EventOptionInstance( new WeekDate(5, 9,0) , new WeekDate(5,11,0)), ])]
+    const ARAT1 = new EventOption(ARAT, 1,[ new EventOptionInstance( new WeekDate(3,13,0) , new WeekDate(3,15,0)), ]);
+    const ARAP1 = new EventOption(ARAP, 1,[ new EventOptionInstance( new WeekDate(2,15,0) , new WeekDate(2,17,0)), ]);
+    const ARAP2 = new EventOption(ARAP, 2,[ new EventOptionInstance( new WeekDate(5,11,0) , new WeekDate(5,13,0)), ]);
+    const ARAP3 = new EventOption(ARAP, 3,[ new EventOptionInstance( new WeekDate(2,17,0) , new WeekDate(2,19,0)), ]);
+    const ARAP4 = new EventOption(ARAP, 4,[ new EventOptionInstance( new WeekDate(5, 9,0) , new WeekDate(5,11,0)), ]);
 
-    ACAT.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(4,10,0) , new WeekDate(4,12,0)), ]), ];
-    ACAP.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(2,11,0) , new WeekDate(2,13,0)), ]),
-                    new EventOption('2',[ new EventOptionInstance( new WeekDate(6,14,0) , new WeekDate(6,16,0)), ]),
-                    new EventOption('3',[ new EventOptionInstance( new WeekDate(2, 9,0) , new WeekDate(2,11,0)), ]),
-                    new EventOption('4',[ new EventOptionInstance( new WeekDate(2,14,0) , new WeekDate(2,16,0)), ])]
+    const ACAP1 = new EventOption(ACAP, 1,[ new EventOptionInstance( new WeekDate(2,11,0) , new WeekDate(2,13,0)), ]);
+    const ACAP2 = new EventOption(ACAP, 2,[ new EventOptionInstance( new WeekDate(6,14,0) , new WeekDate(6,16,0)), ]);
+    const ACAP3 = new EventOption(ACAP, 3,[ new EventOptionInstance( new WeekDate(2, 9,0) , new WeekDate(2,11,0)), ]);
+    const ACAP4 = new EventOption(ACAP, 4,[ new EventOptionInstance( new WeekDate(2,14,0) , new WeekDate(2,16,0)), ]);
+    const ACAT1 = new EventOption(ACAT, 1,[ new EventOptionInstance( new WeekDate(4,10,0) , new WeekDate(4,12,0)), ]);
 
-    CVT.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(3,15,0), new WeekDate(3,17,0)), ]), ];
+    const CVP1 = new EventOption(CVP, 1,[ new EventOptionInstance( new WeekDate(2,15,0), new WeekDate(2,17,0)), ]);
+    const CVP2 = new EventOption(CVP, 2,[ new EventOptionInstance( new WeekDate(2,13,0), new WeekDate(2,15,0)), ]);
+    const CVP3 = new EventOption(CVP, 3,[ new EventOptionInstance( new WeekDate(5,11,0), new WeekDate(5,13,0)), ]);
+    const CVT1 = new EventOption(CVT, 1,[ new EventOptionInstance( new WeekDate(3,15,0), new WeekDate(3,17,0)), ]);
 
-    CVP.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(2,15,0), new WeekDate(2,17,0)), ]),
-                   new EventOption('2',[ new EventOptionInstance( new WeekDate(2,13,0), new WeekDate(2,15,0)), ]),
-                   new EventOption('3',[ new EventOptionInstance( new WeekDate(5,11,0), new WeekDate(5,13,0)), ]),]
+    const EDCP1 = new EventOption(EDCP , 1,[ new EventOptionInstance( new WeekDate(6, 9, 0), new WeekDate(6,11, 0)), ]);
+    const EDCP2 = new EventOption(EDCP , 2,[ new EventOptionInstance( new WeekDate(5,16,30), new WeekDate(5,18,30)), ]);
+    const EDCP3 = new EventOption(EDCP , 3,[ new EventOptionInstance( new WeekDate(6,11, 0), new WeekDate(6,13, 0)), ]);
+    const EDCT1 = new EventOption(EDCT , 1,[ new EventOptionInstance( new WeekDate(5,14,30), new WeekDate(5,16,30)), ]);
 
-    EDCT.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(5,14,30), new WeekDate(5,16,30)), ]), ];
-    EDCP.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(6, 9, 0), new WeekDate(6,11, 0)), ]),
-                    new EventOption('2',[ new EventOptionInstance( new WeekDate(5,16,30), new WeekDate(5,18,30)), ]),
-                    new EventOption('3',[ new EventOptionInstance( new WeekDate(6,11, 0), new WeekDate(6,13, 0)), ]), ];
+    const SEGP1 = new EventOption(SEGP , 1,[ new EventOptionInstance( new WeekDate(5, 9,0), new WeekDate(5,11,0)), ]);
+    const SEGP2 = new EventOption(SEGP , 2,[ new EventOptionInstance( new WeekDate(6,11,0), new WeekDate(6,13,0)), ]);
+    const SEGP3 = new EventOption(SEGP , 3,[ new EventOptionInstance( new WeekDate(6, 9,0), new WeekDate(6,11,0)), ]);
+    const SEGP4 = new EventOption(SEGP , 4,[ new EventOptionInstance( new WeekDate(6,14,0), new WeekDate(6,16,0)), ]);
+    const SEGT1 = new EventOption(SEGT , 1,[ new EventOptionInstance( new WeekDate(4,13,0), new WeekDate(4,15,0)), ]);
 
-    SEGT.options= [ new EventOption('1',[ new EventOptionInstance( new WeekDate(4,13,0), new WeekDate(4,15,0)), ]), ];
-    //const SEGP1 = new EventOption(SEGP , 1,[ new EventOptionInstance( new WeekDate(5, 9,0), new WeekDate(5,11,0)), ]),
-    //const SEGP2 = new EventOption(SEGP , 2,[ new EventOptionInstance( new WeekDate(6,11,0), new WeekDate(6,13,0)), ]),
-    //const SEGP3 = new EventOption(SEGP , 3,[ new EventOptionInstance( new WeekDate(6, 9,0), new WeekDate(6,11,0)), ]),
-    //const SEGP4 = new EventOption(SEGP , 4,[ new EventOptionInstance( new WeekDate(6,14,0), new WeekDate(6,16,0)), ]),];
-
-    //let cl = [
-    //          ARAT, ARAP1, ARAP2, ARAP3, ARAP4,
-    //          ACAT1, ACAP1, ACAP2, ACAP3, ACAP4,
-    //          CVT1, CVP1, CVP2, CVP3,
-    //          EDCT1, EDCP1, EDCP2, EDCP3,
-    //          SEGT1, SEGP1, SEGP2, SEGP3, SEGP4
-    //          ]
-    let cl = [ACAT, ACAP, ARAT, ARAP]
+    let cl = [
+              ARAT1, ARAP1, ARAP2, ARAP3, ARAP4,
+              ACAT1, ACAP1, ACAP2, ACAP3, ACAP4,
+              CVT1, CVP1, CVP2, CVP3,
+              EDCT1, EDCP1, EDCP2, EDCP3,
+              SEGT1, SEGP1, SEGP2, SEGP3, SEGP4
+              ]
 
     let domain = makeDomain(cl)
 
     let sol = search(domain)
-    console.log('Solution', sol)
-  }
+    console.log("BLAAAAAAAAAAAAAAAAAAAAAAAA")
+    console.log(sol)
+}
