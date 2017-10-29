32bd6bb277440302cba929938fd07688b9bc3691
Fábio Maia <fabiu.maia@gmail.com>
2017-10-29 19:08:35 +0000

    Remove stats and firebase

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom'
-import {Stats} from './Stats'
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path='/' component={App} />
-            <Route path='/stats' component={Stats} />
-        </div>
-    </BrowserRouter>, document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
